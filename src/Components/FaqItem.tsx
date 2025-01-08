67753edc6ea0ba55f80ef8f93cd35a69f8e586a6
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-01-08 10:50:19 +0100

    remove borders

--- src/Components/FaqItem.tsx
+++ src/Components/FaqItem.tsx
@@ -9,7 +9,7 @@ interface FaqItemProps {
 
 const Container = styled.div`
   margin: 0;
-  border: 1px solid ${(props) => props.theme.borderColor};
+  //border: 1px solid ${(props) => props.theme.borderColor};
   //border-radius: 0;
   overflow: hidden;
   width: 75vw;
