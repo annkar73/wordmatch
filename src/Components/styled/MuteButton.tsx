eeb7d89965680e3e07a77cc442cad9e270f13915
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-01-15 11:46:08 +0100

    add onTouchStart for toggleMute

--- src/Components/styled/MuteButton.tsx
+++ src/Components/styled/MuteButton.tsx
@@ -24,7 +24,7 @@ export const MuteButton = () => {
   };
 
   return (
-    <MuteButtonStyled onClick={toggleMute}>
+    <MuteButtonStyled onClick={toggleMute} onTouchStart={toggleMute}>
       <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
       {isMuted ? 'Ljud av' : 'Ljud på'}
     </MuteButtonStyled>
